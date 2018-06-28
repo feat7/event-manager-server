6c055efe341af5438da28c3ef0f118023e2a60c5
Vinay Khobragade <vinaykhobragade99@gmail.com>
2018-06-28 19:50:56 +0530

    add env

---
+++ env.config.php
@@ -0,0 +1,19 @@
+<?php 
+    /**
+     * Environment Configuration file
+     */
+
+    /**
+     * App Mode
+     * __DEV__ | __PRODUCTION__
+     */
+
+    const APP_MODE = '__DEV__';
+
+    /**
+     * Database Config
+     */
+    const DB_NAME = 'sql12244965';
+    const DB_USER = 'sql12244965';
+    const DB_PASS = 'P2DgnuI8Kd';
+    const DB_HOST = 'sql12.freemysqlhosting.net';
